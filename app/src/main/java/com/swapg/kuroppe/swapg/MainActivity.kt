274b628b3119fa145034306208806b3e3563c7ee
kuroppe <kuroppems@gmail.com>
2018-03-03 20:34:26 +0900

    Settings of Databinding

--- app/src/main/java/com/swapg/kuroppe/swapg/MainActivity.kt
+++ app/src/main/java/com/swapg/kuroppe/swapg/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.swapg.kuroppe.swapg
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.swapg.kuroppe.swapg.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
