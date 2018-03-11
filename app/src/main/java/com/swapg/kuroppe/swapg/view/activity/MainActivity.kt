8268d33a725ed5ff0b3b51bc5a3d1738b8244f32
kuroppe <kuroppems@gmail.com>
2018-03-12 00:31:09 +0900

    Show sample image into list

--- app/src/main/java/com/swapg/kuroppe/swapg/view/activity/MainActivity.kt
+++ app/src/main/java/com/swapg/kuroppe/swapg/view/activity/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.swapg.kuroppe.swapg.view.activity
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import com.swapg.kuroppe.swapg.R
 import com.swapg.kuroppe.swapg.databinding.ActivityMainBinding
+import com.swapg.kuroppe.swapg.view.item.PostItem
 import com.swapg.kuroppe.swapg.viewmodel.MainViewModel
 import com.swapg.kuroppe.swapg.viewmodel.ViewModelFactory
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Section
+import com.xwray.groupie.ViewHolder
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
 
     @Inject lateinit var viewModelFactory: ViewModelFactory
+
+    private val postSection = Section()
 
     private val binding: ActivityMainBinding by lazy {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
@@ -24,6 +31,11 @@ class MainActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel.hoge()
+        binding.postRecyclerView.adapter = GroupAdapter<ViewHolder>().also { it.add(postSection) }
+        viewModel.posts().observe(this, Observer { list ->
+            list?.let {
+                postSection.update(it.map { post -> PostItem(post) })
+            }
+        })
     }
 }
